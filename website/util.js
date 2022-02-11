7e4d6a34eef21dd4787be2faae5e1ac05dee7e0d
Moritz Biering <moritzbiering.mb@gmail.com>
2022-02-11 18:24:47 +0100

    Improve appearance

--- website/util.js
+++ website/util.js
@@ -4,17 +4,11 @@ var exec = promisify(require('child_process').exec);
 var config = require('./config');
 
 async function loadDirs() {
-    // return ['TestDir1', 'MyDir2', 'Directory'];
-
     const { stdout, stderr } = await exec(config.scriptPath + 'directory.sh load');
     return stdout.split(';');
 }
 
 async function loadImgs() {
-    // return ['image01', 'image02', 'image03',
-    //     'image04', '_', 'image05', 'image06', '_',
-    //     'image07', 'image08', 'image09'].join(';')
-
     const { stdout, stderr } = await exec(config.scriptPath + 'image.sh load');
     return stdout;
 }
