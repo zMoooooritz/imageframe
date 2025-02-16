e00df444d0060433e4106c8289ec298777a0f6ec
Moritz Biering <moritzbiering.mb@gmail.com>
2025-02-16 18:44:25 +0100

    feat: ui overhaul

--- routes/screen.js
+++ routes/screen.js
@@ -2,20 +2,14 @@ const express = require('express');
 const router = express.Router();
 const system = require('../util/system');
 
-router.get('/', function(req, res, next) {
-    res.render('screen', { title: res.__('Screen') });
-});
-
 router.post('/on', async function(req, res, next) {
     system.displayOn();
-
-    res.redirect('/screen');
+    res.redirect('/');
 });
 
 router.post('/off', async function(req, res, next) {
     system.displayOff();
-
-    res.redirect('/screen');
+    res.redirect('/');
 });
 
 module.exports = router;
