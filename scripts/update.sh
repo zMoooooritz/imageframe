e0284cfb46f0813e81e793ccd13ca99264a055f4
Moritz Biering <moritzbiering.mb@gmail.com>
2025-02-16 22:19:26 +0100

    feat: improved update mechanism

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
-git fetch
-git pull --force
+# get new tags from remote
+git fetch --tags
+
+# get latest tag name
+latestTag=$(git describe --tags "$(git rev-list --tags --max-count=1)")
+
+# checkout latest tag
+git checkout $latestTag
+
+# update node dependencies
+npm install
+
+# restart in order for all changes to take effect
 sudo shutdown -r now
